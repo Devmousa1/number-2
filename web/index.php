bd4597d33ca26da330be2740a87c5ccd14a8481c
Mohammed Foud <mfoud444@gmail.com>
2024-10-14 00:56:16 +0300

    first commit

--- web/index.php
+++ web/index.php
@@ -1,9 +1,9 @@
 <?php
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-error_log('Script started');
+
+echo('Script started');
 date_default_timezone_set('Asia/Aden');
 
 include_once 'regEx.php';
@@ -17,7 +17,7 @@ $url            = "https://api.telegram.org/bot{$bot_API_KEY}/setWebHook?url={$m
 $response = file_get_contents($url);
 
 // Log webhook response
-error_log('Webhook set response: ' . $response);
+echo('Webhook set response: ' . $response);
 include_once "telegram_bot.php";
 /*
 if(preg_match("/^\{/",$argv[1])){
@@ -26,15 +26,15 @@ if(preg_match("/^\{/",$argv[1])){
 */
 
 $update = json_decode(file_get_contents('php://input'));
-error_log('Update received: ' . print_r($update, true));
+echo('Update received: ' . print_r($update, true));
 
 if($update != null){
 	include_once 'variables.php';
 	include_once 'mybot.php';
-	error_log('Bot logic executed');
+	echo('Bot logic executed');
 	exit;
 }
-error_log('No update received, exiting.');
+echo('No update received, exiting.');
 exit;
 
 
